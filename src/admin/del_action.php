1c50d1bf28b742ba775c087d3a3670f5abb5dcee
Benno Lang <transmogribenno@gmail.com>
2022-03-05 14:16:04 +1030

    Remove trailing spaces

--- src/admin/del_action.php
+++ src/admin/del_action.php
@@ -34,21 +34,21 @@ $table_single_name = $table->getNameSingle ();
 // Load alternate names if they have been specified
 if (@$_POST['_p']) {
     $ancestors = explode (',', $_POST['_p']);
-    
+
     if (count($ancestors) > 0) {
         list ($parent_table) = explode ('.', $ancestors[0]);
-        
+
         $parent_table = $db->getTable ($parent_table);
         if ($parent_table == null) {
             report_error ("Invalid ancestor {$ancestor_name}");
             die();
         }
-        
+
         $col = $table->getLinkToTable ($parent_table);
-        
+
         $alt_name = $col->getLink ()->getAltEngName ();
         if ($alt_name) $table_eng_name = $alt_name;
-        
+
         // Ready for the future
         //$alt_name = $col->getLink()->getAltSingleName();
         //if ($alt_name) $table_single_name = $alt_name;
@@ -56,27 +56,27 @@ if (@$_POST['_p']) {
 }
 
 
-    
+
 // delete selected rows
 if (@count ($_POST['del']) > 0) {
-    
+
     $record_pks = array ();
     foreach ($_POST['del'] as $item_to_del => $val) {
         if ($val == 1) {
             $record_pks[] = explode (',', $item_to_del);
         }
     }
-    
+
     // do the delete
     $num_removed = $table->deleteRecords ($record_pks);
-    
+
     // confirmation message
     switch ($num_removed) {
         case 0: $_SESSION[ADMIN_KEY]['err'] = "Deletion failed"; break;
         case 1: $_SESSION[ADMIN_KEY]['msg'] = "1 {$table_single_name} deleted successfully"; break;
         default: $_SESSION[ADMIN_KEY]['msg'] = "{$num_removed} {$table_eng_name} deleted successfully"; break;
     }
-    
+
 } else {
     // user didnt select anything
     $_SESSION[ADMIN_KEY]['err'] = "No {$table_eng_name} were selected for deletion!";
