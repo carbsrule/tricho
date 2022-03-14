c82723772bb6319b3540122e3a318112481ebfff
Benno Lang <transmogribenno@gmail.com>
2022-03-14 22:32:06 +1030

    Fix bad path detection in example config

--- src/tricho/config/examples/detect.php
+++ src/tricho/config/examples/detect.php
@@ -7,7 +7,7 @@
 
 $_detect_uname = php_uname('n');
 $_detect_uname = preg_replace('/[^a-z0-9_\-\.]/i', '', $_detect_uname);
-if (file_exists($_detect_uname . '.php')) {
+if (file_exists(__DIR__ . "/{$_detect_uname}.php")) {
     require __DIR__ . "/{$_detect_uname}.php";
 } else {
     require __DIR__ . '/dev.php';
