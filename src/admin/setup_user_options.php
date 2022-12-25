a0cfbfd96cb04dde6ae2e2e1cc284b333624dc6b
Benno Lang <transmogribenno@gmail.com>
2022-12-26 07:38:11 +1030

    Remove superfluous trailing spaces

--- src/admin/setup_user_options.php
+++ src/admin/setup_user_options.php
@@ -6,20 +6,20 @@
  */
 
 function show_setup_user_option ($english, $opt) {
-    
+
     $current_status = (@$_SESSION['setup']["view_{$opt}"]? 'y': 'n');
     $new_status = ($current_status == 'y'? 'n': 'y');
-    
+
     $url = $_SERVER['REQUEST_URI'];
     $replacements = array (
         "?{$opt}={$current_status}&" => '?',
         "?{$opt}={$current_status}" => '',
         "&{$opt}={$current_status}" => ''
     );
-    
+
     $url = htmlspecialchars (str_replace (array_keys ($replacements), $replacements, $url));
     $url .= ((strpos ($url, '?') === false)? "?{$opt}={$new_status}": "&amp;{$opt}={$new_status}");
-    
+
     echo "<label class=\"label_plain\" for=\"admin_opt_{$opt}\">";
     echo "<input id=\"admin_opt_{$opt}\" type=\"checkbox\" value=\"y\" onclick=\"window.location = '",
         get_proto_host (), $url, "';\"";
@@ -30,13 +30,13 @@ function show_setup_user_option ($english, $opt) {
 }
 
 if (test_setup_login (false, SETUP_ACCESS_FULL)) {
-    
+
     if (@$_GET['q'] == 'y') {
         $_SESSION['setup']['view_q'] = true;
     } else if (@$_GET['q'] == 'n') {
         $_SESSION['setup']['view_q'] = false;
     }
-    
+
     if (@$_GET['c'] == 'y') {
         $_SESSION['setup']['view_c'] = true;
     } else if (@$_GET['c'] == 'n') {
@@ -45,12 +45,12 @@ if (test_setup_login (false, SETUP_ACCESS_FULL)) {
 }
 
 if (test_setup_login (false, SETUP_ACCESS_LIMITED)) {
-    
+
     if (@$_GET['h'] == 'y') {
         $_SESSION['setup']['view_h'] = true;
     } else if (@$_GET['h'] == 'n') {
         $_SESSION['setup']['view_h'] = false;
     }
-    
+
 }
 ?>
